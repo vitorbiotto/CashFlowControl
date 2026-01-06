0cca40070e70f765a4260712b5ee1cdf18bc6836
vitorbiotto <vitor.biotto@hotmail.com>
2026-01-05 21:26:53 -0300

    First commit

---
+++ src/main/java/com/example/CashFlowControl/CashFlowControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.CashFlowControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CashFlowControl/CashFlowControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CashFlowControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
